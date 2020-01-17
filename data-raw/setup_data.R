4abda78aea2691de2bd43825b5b52f16c2ba41b6
Julia Silge <julia.silge@gmail.com>
2020-01-16 21:57:43 -0700

    Don't need the complaints with no text

--- data-raw/setup_data.R
+++ data-raw/setup_data.R
@@ -2,5 +2,6 @@ library(tidyverse)
 
 read_csv("data-raw/complaints.csv") %>%
   janitor::clean_names() %>%
-  filter(date_received >= "2019-01-01") %>%
+  filter(date_received >= "2019-01-01",
+         !is.na(consumer_complaint_narrative)) %>%
   write_csv("data/complaints.csv.gz")
