ca1ab156108297089c4ed79aa2404cec369e8d95
Julia Silge <julia.silge@gmail.com>
2020-01-16 21:30:41 -0700

    Clean the CFPB names

--- data-raw/setup_data.R
+++ data-raw/setup_data.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
 read_csv("data-raw/complaints.csv") %>%
-  filter(`Date received` >= "2019-01-01") %>%
+  janitor::clean_names() %>%
+  filter(date_received >= "2019-01-01") %>%
   write_csv("data/complaints.csv.gz")
