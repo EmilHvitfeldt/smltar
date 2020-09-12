3952f34709606fa819936ca678d3c80a209f170a
Julia Silge <julia.silge@gmail.com>
2020-09-12 16:38:36 -0600

    Updates to data-raw script

--- data-raw/setup_data.R
+++ data-raw/setup_data.R
@@ -1,3 +1,5 @@
+### CFPB complaints
+
 library(tidyverse)
 
 read_csv("data-raw/complaints.csv") %>%
@@ -5,3 +7,16 @@ read_csv("data-raw/complaints.csv") %>%
   filter(date_received >= "2019-01-01",
          !is.na(consumer_complaint_narrative)) %>%
   write_csv("data/complaints.csv.gz")
+
+### DOJ press releases
+
+library(jsonlite)
+library(tidyverse)
+
+out <- stream_in(file("data-raw/combined.json")) %>% 
+  as_tibble() %>%
+  mutate(agency = map_chr(components, `[`, 1)) %>%
+  select(date, agency, title, contents)
+
+out %>%
+  write_csv("data/press_releases.csv.gz")
