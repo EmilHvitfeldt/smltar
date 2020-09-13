eb8b73098e010057353862341e6326f404f14352
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2020-09-12 23:14:59 -0700

    add kickstarter data

--- data-raw/setup_data.R
+++ data-raw/setup_data.R
@@ -21,3 +21,14 @@ out <- stream_in(file("data-raw/combined.json")) %>%
 
 out %>%
   write_csv("data/press_releases.csv.gz")
+
+### Kickstarter blurbs
+# https://webrobots.io/kickstarter-datasets/
+
+dir_ls(c("~/Downloads/Kickstarter_2016-01-28T09_15_08_781Z", 
+         "~/Downloads/Kickstarter_2016-03-22T07_41_08_591Z/")) %>%
+  map_dfr(read_csv, col_types = cols_only(blurb = col_character(),
+                                          state = col_character())) %>%
+  filter(state %in% c("failed", "successful")) %>%
+  mutate(state = as.numeric(state == "successful")) %>%
+  write_csv("data/kickstarter.csv.gz")
