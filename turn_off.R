ea6fb6383efccb13e1cb48b619de6682939ed3b9
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-01-09 19:49:59 -0800

    add single_core() function

--- turn_off.R
+++ turn_off.R
@@ -7,3 +7,11 @@ turn_off <- function(x) {
 }
 
 purrr::walk(fs::dir_ls(regexp = "[0-9]+.*\\.Rmd$"), turn_off)
+
+single_core <- function(x) {
+  text <- readLines(x)
+  text <- gsub(pattern = "doParallel::registerDoParallel()", replace = "#doParallel::registerDoParallel()", x = text)
+  writeLines(text, x)
+}
+
+#purrr::walk(fs::dir_ls(regexp = "[0-9]+.*\\.Rmd$"), single_core)
