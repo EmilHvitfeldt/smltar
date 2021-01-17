8fe5167e075da07e829e6fd01d0b0655a85d71b4
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-01-16 17:44:15 -0800

    better turn_off()

--- turn_off.R
+++ turn_off.R
@@ -2,6 +2,7 @@ turn_off <- function(x) {
   text <- readLines(x)
   text <- gsub(pattern = ", eval=\\!?online", replace = "", x = text)
   text <- gsub(pattern = "`r ", replace = "`r #", x = text)
+  text <- gsub(pattern = "`r #if (", replace = "`r if (", x = text)
   text <- gsub(pattern = "eval ?= ?TRUE", replace = "eval=FALSE", x = text)
   writeLines(text, x)
 }
