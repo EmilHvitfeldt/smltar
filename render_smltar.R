094c7deec0f90852e0322c9ed18688085be23064
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-04-02 11:52:52 -0700

    use python_version in conda_create()

--- render_smltar.R
+++ render_smltar.R
@@ -8,7 +8,7 @@ devtools::install_dev_deps()
 
 ## assume Miniconda installed and R environment exists called `r-reticulate`:
 # reticulate::install_miniconda()
-# reticulate::conda_create('r-reticulate', packages = c('python==3.6.9'))
+# reticulate::conda_create('r-reticulate', python_version = "3.6.9")
 
 keras::install_keras(tensorflow = '2.2', extra_packages = c('IPython', 'requests', 'certifi', 'urllib3'))
 spacyr::spacy_install(python_version = "3.6.9", envname = "spacy_condaenv", prompt = FALSE)
