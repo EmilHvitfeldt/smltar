f1b4bc0756269a235e9af4c44c0633b6373b16d2
EmilHvitfeldt <emilhhvitfeldt@gmail.com>
2021-10-09 11:07:55 -0700

    recalculate tidy_pmi

---
+++ inst/tidy_pmi.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+library(tidytext)
+
+complaints <- read_csv("data/complaints.csv.gz")
+
+tidy_complaints <- complaints %>%
+  select(complaint_id, consumer_complaint_narrative) %>%
+  unnest_tokens(word, consumer_complaint_narrative) %>%
+  add_count(word) %>%
+  filter(n >= 50) %>%
+  select(-n)
+
+nested_words <- tidy_complaints %>%
+  nest(words = c(word))
+
+slide_windows <- function(tbl, window_size) {
+  skipgrams <- slider::slide(
+    tbl,
+    ~.x,
+    .after = window_size - 1,
+    .step = 1,
+    .complete = TRUE
+  )
+  
+  safe_mutate <- safely(mutate)
+  
+  out <- map2(skipgrams,
+              1:length(skipgrams),
+              ~ safe_mutate(.x, window_id = .y))
+  
+  out %>%
+    transpose() %>%
+    pluck("result") %>%
+    compact() %>%
+    bind_rows()
+}
+
+library(widyr)
+library(furrr)
+
+plan(multisession)  ## for parallel processing
+
+tidy_pmi <- nested_words %>%
+  mutate(words = future_map(words, slide_windows, 4L)) %>%
+  unnest(words) %>%
+  unite(window_id, complaint_id, window_id) %>%
+  pairwise_pmi(word, window_id)
+
+save(tidy_pmi, file = "data/tidy_pmi.rda")
